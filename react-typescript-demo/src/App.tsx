ec72a89e1ba479cf3d7f5394d54b84eb20146cfb
unknown <kunalsharmarok@gmail.com>
2025-08-22 19:21:35 +0530

    generic props typescript

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,15 +1,41 @@
 import './App.css';
-import { Private } from './components/auth/Private';
-import { Profile } from './components/auth/Profile';
+import { List } from './components/generics/List';
+
 
 
 function App() {
 
-  return <div className="App">
+  return (
+    <div className="App">
+    {/* <List
+        items={['Batman', 'Superman', 'Wonder Woman']}
+        onClick={(item) => console.log(item)}
+      />
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          {
+          id : 1,
+          first: 'Clark',
+          last: 'Kent',
 
-    <Private isLoggedIn={true} component={Profile}/>
+        },
+          {
+           id: 2,
+          first: 'Bruce',
+          last: 'Wayne',
+
+        },
+          {
+           id: 3,
+          first: 'Princess',
+          last: 'Diana',
+        },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
   </div>
-
+      )
 }
 
 export default App;
