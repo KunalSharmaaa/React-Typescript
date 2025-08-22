b3d08319ea18b3f87f1f220ec143092b0d7c43cc
unknown <kunalsharmarok@gmail.com>
2025-08-22 18:09:16 +0530

    typescript component props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css';
+import { Private } from './components/auth/Private';
+import { Profile } from './components/auth/Profile';
+
 
 function App() {
 
   return <div className="App">
+
+    <Private isLoggedIn={true} component={Profile}/>
   </div>
 
 }
